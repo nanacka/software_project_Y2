ff1564761f8dcc11071f4bb7d54e1ce7eff4315c
n00222614 <n00222614@iadt.ie>
2024-04-11 16:47:58 +0100

    working on views and images

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -22,39 +22,9 @@ class PostController extends Controller
 
     public function show(string $id)
     {
-        // $posts = Post::findOrFail($id);
-        // return view('user.posts.show')->with('posts', $posts);
-        
+
         $post = Post::findOrFail($id);
 
-        //need to get all of the comments where the post id matches the id here
-        //get all comments where id = id up there
-
-
-        // $comments = Post::find($id);
-        // $comments = Comment::findOrFail($comments->post_id);
-
-        // $comments = Comment::select('')
-
-        // $comments = DB::table('comments')->where('post_id', $id)->get('content', 'user_id');
-
-        // $commenters = DB::table('comments')->leftJoin('users', 'comments.user_id', '=', 'users.name')->where('comments.post_id', $id)->get('user_id as users.name');
-
-        // $comments = DB::table('comments.content')->select('content')->where('comments.post_id', $id);
-
-        // $post
-
-        // 'user_id as id'
-
-        // $commenters = DB::table('comments.user_id')->where()
-
-        //getting username. got to get the user first name. foreign id on the posts is user id. hmmm
-
-        // if($comments){
-        //     return view('user.posts.show', [
-        //         'post' => $post
-        //     ]);
-        // }
         
 
         return view('user.posts.show', [
